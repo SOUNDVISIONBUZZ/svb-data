9524dc47d8ce38eb1d1342410bc33c9d0a945814
SOUNDVISIONBUZZ <soundvisionbuzz@gmail.com>
2025-08-07 12:27:54 -0700

    fix: correct import in __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,2 +1,9 @@
-from .livenotessb import fetch as lnsb_fetch
+# sources/__init__.py
+"""
+Export all fetcher functions
+"""
 
+from .lnsb_fetch import lnsb_fetch
+
+# Export other fetchers if they exist
+# from .other_source import other_fetch
